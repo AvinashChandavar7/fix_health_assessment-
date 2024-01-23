ce5f5290260febcb34532791014bb2514e68e318
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-01-24 01:14:42 +0530

    feat : add  constants data

--- src/api/doctors.js
+++ src/api/doctors.js
@@ -1,17 +1,13 @@
+import { doctorsData } from "../constants/data";
+
 // eslint-disable-next-line no-unused-vars
 export const getDoctors = async (city) => {
   return new Promise(resolve => {
     setTimeout(() => {
-      const doctorsData = [
-        { name: 'Dr. Smith', expertise: 'Physiotherapy', city: 'New York' },
-        { name: 'Dr. Johnson', expertise: 'Sports Medicine', city: 'Chicago' },
-        { name: 'Dr. sacassac', expertise: 'Sports Medicine', city: 'Chicago' },
-        { name: 'Dr. Williams', expertise: 'Rehabilitation', city: 'Los Angeles' },
-        { name: 'Dr. Brown', expertise: 'Orthopedic', city: 'Houston' },
-      ];
-
       // Filter doctors by city if a city is provided
-      const filteredDoctors = city ? doctorsData.filter(doctor => doctor.city === city) : doctorsData;
+      const filteredDoctors = city
+        ? doctorsData.filter(doctor => doctor.city === city)
+        : doctorsData;
 
       resolve(filteredDoctors);
     }, 1000);
