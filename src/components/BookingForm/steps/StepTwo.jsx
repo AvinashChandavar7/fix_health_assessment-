bcceb4b46f549e9aa56d66d0066f3910ae178946
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-01-24 01:29:47 +0530

    fix : age input field

--- src/components/BookingForm/steps/StepTwo.jsx
+++ src/components/BookingForm/steps/StepTwo.jsx
@@ -14,7 +14,7 @@ const StepTwo = () => {
       {/* Age */}
       <div className='flex flex-col my-3'>
         <label>Age<span className='text-red-500'>*</span></label>
-        <input {...register("age", { required: 'Age is required' })} className='bg-transparent border px-4 py-2 rounded-md' />
+        <input {...register("age", { required: 'Age is required' })} type='number' className='bg-transparent border px-4 py-2 rounded-md' />
         {errors.age && <p>{errors.age.message}</p>}
       </div>
       {/* City dropdown */}
